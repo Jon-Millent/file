6080c9ad0fa73ed77fb07950b7ea125ede9f4c4e
millent <millent@126.com>
2017-04-20 08:25:41 +0800

    Update tool.js

--- tool.js
+++ tool.js
@@ -1,30 +1,39 @@
-//获取url后的参数
-function GetRequest(names) {   
-     var url = location.search; //获取url中"?"符后的字串   
-     var theRequest = new Object();   
-     if (url.indexOf("?") != -1) {   
-        var str = url.substr(1);   
-        strs = str.split("&");   
-        for(var i = 0; i < strs.length; i ++) {   
-           theRequest[strs[i].split("=")[0]]=unescape(strs[i].split("=")[1]);   
-        }   
-     }   
-     return names ? theRequest[name] : theRequest;   
-  } 
-//获取两日期之间相差的天数
-function getNowDays(time){
-     var ttime = new Date(time);
-     var ntime = new Date();
+(function (root,factory) {
+	
+	root.millent = {
+		//获取url后的参数
+		getRequest : function (names) {   
+		     var url = location.search; //获取url中"?"符后的字串   
+		     var theRequest = new Object();   
+		     if (url.indexOf("?") != -1) {   
+		        var str = url.substr(1);   
+		        strs = str.split("&");   
+		        for(var i = 0; i < strs.length; i ++) {   
+		           theRequest[strs[i].split("=")[0]]=unescape(strs[i].split("=")[1]);   
+		        }   
+		     }   
+		     return names ? theRequest[name] : theRequest;   
+		}, 
+		//获取两日期之间相差的天数
+		getNowDays : function(time){
+		     var ttime = new Date(time);
+		     var ntime = new Date();
 
-     var best = new Date();
+		     var best = new Date();
 
-     best.setTime(ttime.getTime() - ntime.getTime());
+		     best.setTime(ttime.getTime() - ntime.getTime());
 
-     return best.getTime() / 86400000;
+		     return Math.floor(best.getTime() / 86400000);
+		},
+		//获取本地化时间
+		getLocationTime : function(time){
+		     var gtime = new Date(time || new Date());
+		     return gtime.getFullYear() + '年' + (gtime.getMonth()+1) + '月' + gtime.getDay() +　'日'
+		}
+	}
 
-}
-//获取本地化时间
-function getLocationTime(time){
-     var gtime = new Date(time);
-     return gtime.getFullYear() + '年' + (gtime.getMonth()+1) + '月' + gtime.getDay() +　'日'
-}
+})(window,(function(){
+
+	console.log('do your want to do');
+
+})())
