8df8f3db4fce8942fdefb8493c36c273f48a7c4c
millent <millent@126.com>
2017-06-25 21:22:16 +0800

    Update tool.js

--- tool.js
+++ tool.js
@@ -1,45 +1,33 @@
-(function (root,factory) {
+;(function (root,$,plus) {
 	
-	root.millent = {
-		//获取url后的参数
-		getRequest : function (names) {   
-		     var url = location.search; //获取url中"?"符后的字串   
-		     var theRequest = new Object();   
-		     if (url.indexOf("?") != -1) {   
-		        var str = url.substr(1);   
-		        strs = str.split("&");   
-		        for(var i = 0; i < strs.length; i ++) {   
-		           theRequest[strs[i].split("=")[0]]=unescape(strs[i].split("=")[1]);   
-		        }   
-		     }   
-		     return names ? theRequest[name] : theRequest;   
-		}, 
-		//获取两日期之间相差的天数
-		getNowDays : function(time){
-		     var ttime = new Date(time);
-		     var ntime = new Date();
-
-		     var best = new Date();
-
-		     best.setTime(ttime.getTime() - ntime.getTime());
-
-		     return Math.floor(best.getTime() / 86400000);
+	root.rose={
+		url : '192.168.0.66/',
+		util : {//在这里拓展工具方法
+			random : function(lowValue,highValue){
+		        var choice=highValue-lowValue+1;
+		        return Math.floor(Math.random()*choice+lowValue);
+		    },
+		    getReauest : function() {   
+			   var url = location.search; //获取url中"?"符后的字串
+			   var theRequest = new Object();   
+			   if (url.indexOf("?") != -1) {   
+			      var str = url.substr(1);   
+			      strs = str.split("&");   
+			      for(var i = 0; i < strs.length; i ++) {   
+			         theRequest[strs[i].split("=")[0]]=decodeURIComponent(strs[i].split("=")[1]);   
+			      }   
+			   }   
+			   return theRequest;   
+			}   
 		},
-		//获取本地化时间
-		getLocationTime : function(time){
-		     var gtime = new Date(time || new Date());
-		     return gtime.getFullYear() + '年' + (gtime.getMonth()+1) + '月' + gtime.getDay() +　'日'
-		},
-		iGetInnerText:function(testStr) {
-			var resultStr = testStr.replace(/\ +/g, ""); //去掉空格
-			resultStr = testStr.replace(/[ ]/g, "");    //去掉空格
-			resultStr = testStr.replace(/[\r\n]/g, ""); //去掉回车换行
-			return resultStr;
-	    	},
-		getRandomNumber:function(a,b){
-			return Math.ceil((Math.random()*(b-a)-1)+a);
+		ajax : {//在这里封装ajax
+
 		}
 	}
+
+
+
+	/* Fomate Time */
 	Date.prototype.Format = function (fmt) { //author: meizz
 	    var o = {
 		"M+": this.getMonth() + 1, //月份
@@ -55,8 +43,126 @@
 		if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
 	    return fmt;
 	}
-})(window,(function(){
 
-	console.log('do your want to do');
 
-})())
+})(window,jQuery,(function(){
+
+/*!
+ * jQuery Cookie Plugin v1.4.1
+ * https://github.com/carhartl/jquery-cookie
+ *
+ * Copyright 2013 Klaus Hartl
+ * Released under the MIT license
+ */
+(function (factory) {
+	if (typeof define === 'function' && define.amd) {
+		// AMD
+		define(['jquery'], factory);
+	} else if (typeof exports === 'object') {
+		// CommonJS
+		factory(require('jquery'));
+	} else {
+		// Browser globals
+		factory(jQuery);
+	}
+}(function ($) {
+
+	var pluses = /\+/g;
+
+	function encode(s) {
+		return config.raw ? s : encodeURIComponent(s);
+	}
+
+	function decode(s) {
+		return config.raw ? s : decodeURIComponent(s);
+	}
+
+	function stringifyCookieValue(value) {
+		return encode(config.json ? JSON.stringify(value) : String(value));
+	}
+
+	function parseCookieValue(s) {
+		if (s.indexOf('"') === 0) {
+			// This is a quoted cookie as according to RFC2068, unescape...
+			s = s.slice(1, -1).replace(/\\"/g, '"').replace(/\\\\/g, '\\');
+		}
+
+		try {
+			// Replace server-side written pluses with spaces.
+			// If we can't decode the cookie, ignore it, it's unusable.
+			// If we can't parse the cookie, ignore it, it's unusable.
+			s = decodeURIComponent(s.replace(pluses, ' '));
+			return config.json ? JSON.parse(s) : s;
+		} catch(e) {}
+	}
+
+	function read(s, converter) {
+		var value = config.raw ? s : parseCookieValue(s);
+		return $.isFunction(converter) ? converter(value) : value;
+	}
+
+	var config = $.cookie = function (key, value, options) {
+
+		// Write
+
+		if (value !== undefined && !$.isFunction(value)) {
+			options = $.extend({}, config.defaults, options);
+
+			if (typeof options.expires === 'number') {
+				var days = options.expires, t = options.expires = new Date();
+				t.setTime(+t + days * 864e+5);
+			}
+
+			return (document.cookie = [
+				encode(key), '=', stringifyCookieValue(value),
+				options.expires ? '; expires=' + options.expires.toUTCString() : '', // use expires attribute, max-age is not supported by IE
+				options.path    ? '; path=' + options.path : '',
+				options.domain  ? '; domain=' + options.domain : '',
+				options.secure  ? '; secure' : ''
+			].join(''));
+		}
+
+		// Read
+
+		var result = key ? undefined : {};
+
+		// To prevent the for loop in the first place assign an empty array
+		// in case there are no cookies at all. Also prevents odd result when
+		// calling $.cookie().
+		var cookies = document.cookie ? document.cookie.split('; ') : [];
+
+		for (var i = 0, l = cookies.length; i < l; i++) {
+			var parts = cookies[i].split('=');
+			var name = decode(parts.shift());
+			var cookie = parts.join('=');
+
+			if (key && key === name) {
+				// If second argument (value) is a function it's a converter...
+				result = read(cookie, value);
+				break;
+			}
+
+			// Prevent storing a cookie that we couldn't decode.
+			if (!key && (cookie = read(cookie)) !== undefined) {
+				result[name] = cookie;
+			}
+		}
+
+		return result;
+	};
+
+	config.defaults = {};
+
+	$.removeCookie = function (key, options) {
+		if ($.cookie(key) === undefined) {
+			return false;
+		}
+
+		// Must not alter options, thus extending a fresh object...
+		$.cookie(key, '', $.extend({}, options, { expires: -1 }));
+		return !$.cookie(key);
+	};
+
+}));
+	
+})());
