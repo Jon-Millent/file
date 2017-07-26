199bf3d467b49b37acc02eb421ae0cc43df78220
millent <millent@126.com>
2017-07-26 18:04:18 +0800

    为vue添加tip指令

---
+++ v-tip.js
@@ -0,0 +1,38 @@
+	//封装自定义指令 tip
+	Vue.directive('tap', {
+		update : function(){
+			console.log('ssssss')
+		},
+		bind : function(ele,data){
+			var dataTime = 0;
+			var defaultTime = 100;
+			var defaultPx = 10;
+			var x = 0;
+			var y = 0;
+
+			ele.addEventListener('touchstart',function(e){
+				x = e.changedTouches[0].clientX;
+				y = e.changedTouches[0].clientY;
+				dataTime = new Date();
+			})
+			ele.addEventListener('touchend',function(e){
+				var endTime = (new Date() - dataTime);
+
+				var endX = Math.abs(x - e.changedTouches[0].clientX);
+				var endY = Math.abs(y - e.changedTouches[0].clientY);
+
+				if(endTime <= defaultTime || (endX <= defaultPx && endY <= defaultPx)){
+					data.value();
+
+					if(data.modifiers.prevent){
+						e.preventDefault();
+						console.log('prevent')
+					}
+					if(data.modifiers.stop){
+						e.stopPropagation();
+						console.log('stop')
+					}
+				}
+			})
+		}
+	})
