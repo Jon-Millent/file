95a28266f2e3a6c6ff2b1c3065a01bb66eb6f5a4
millent <millent@126.com>
2017-09-19 19:17:44 +0800

    移动端阻止滚动
    移动端阻止滚动

--- stopScroll.js
+++ stopScroll.js
@@ -1,18 +1,18 @@
-	scrollUtil : {
-		stopScroll : function(){
-		    var a = document.body.style;
-		    a.position = "fixed";
-		    a.left = 0;
-		    a.right = 0;
-		    a.bottom = 0;
-		    a.top = 0;
+scrollUtil : {
+	stopScroll : function(){
+	    var a = document.body.style;
+	    a.position = "fixed";
+	    a.left = 0;
+	    a.right = 0;
+	    a.bottom = 0;
+	    a.top = 0;
 
-		},
-		runScroll : function(){
-		    document.body.style.position='';
-		    document.body.style.left='';
-		    document.body.style.right='';
-		    document.body.style.bottom='';
-		    document.body.style.top='';
-		}
+	},
+	runScroll : function(){
+	    document.body.style.position='';
+	    document.body.style.left='';
+	    document.body.style.right='';
+	    document.body.style.bottom='';
+	    document.body.style.top='';
 	}
+}
